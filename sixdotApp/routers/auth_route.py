2e4b469e3a99d466ae1eeec199879781a6e7d317
veenzent <odumevincent19@gmail.com>
2024-10-29 12:54:54 +0100

    account generation route completed

--- sixdotApp/routers/auth_route.py
+++ sixdotApp/routers/auth_route.py
@@ -1,57 +1,43 @@
-from typing_extensions import deprecated
 from fastapi import APIRouter, Request
 # from cachetools import TTLCache, cached
-from datetime import timedelta
 from .. import schemas, models, dependencies
 from ..instance.config import get_settings
-from algosdk import kmd, wallet
+from algosdk import kmd, wallet, account, mnemonic
 
 
 auth_route = APIRouter()
 # cache = TTLCache(maxsize=100, ttl=300)
 
-# First, set up a connection to the KMD client
+# Set up a connection to the KMD client
 kmd_address = get_settings().kmd_address
 kmd_token = get_settings().kmd_token
 kmd_client = kmd.KMDClient(kmd_token=kmd_token, kmd_address=kmd_address)
 
 
-@auth_route.post("/create_wallet")
-async def create_wallet(request: Request, wallet_request: models.WalletRequest):
-    """Creates wallet for new users
-
-    Args:
-        request (Request): _description_
-        wallet_request (models.WalletRequest): Pydantic basemodel to get new users wallet name and password
+@auth_route.post("/generate_account")
+async def generate_account():
+    """Generate an account and returns wallet address and passphrase
 
     Returns:
-        dict: json response
+        dict: json response of wallet information
     """
-    #  get wallet name and password
-    wallet_name = wallet_request.wallet_name
-    wallet_password = wallet_request.password
+    # generate an account
+    private_key, address = account.generate_account()
 
-    if wallet_password:
-        # hash password
-        hashed_pwd = dependencies.hash_password(wallet_password)
-        
-        # create new wallet
-        new_wallet = wallet.Wallet(wallet_name, wallet_password, kmd_client)
-
-        # Get wallet information
-        info = new_wallet.info()
-        print(f"Wallet name: {info['wallet']['name']}")
-
-        # Optionally, you can get the mnemonic for the master derivation key
-        backup = new_wallet.get_mnemonic()
-        # print(f"Mnemonic for master derivation key: {backup}")
+    # get wallet passphrase from private key
+    passphrase = mnemonic.from_private_key(private_key)
+    passphrase = passphrase.split(" ")
+    indices =range(1, len(passphrase)+1, 1)
+    seed_passphrase = dict(zip(indices, passphrase))
 
     return {
         "message": "Wallet created successfully",
-        "Wallet name": info['wallet']['name'],
-        "Passphrase": backup
+        "wallet address": address,
+        "passphrase": seed_passphrase
     }
 
-@auth_route.post("/signin")
-async def sign_in():
+@auth_route.post("/import_wallet")
+async def import_wallet():
+    """Not functional yet
+    """
     pass
